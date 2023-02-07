95104d85a76d537fc66bb26ad522b394d65b8b8c
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-02-06 21:10:02 -0800

    commit tree 73b1d394702f1720bd17c14d7ecace12a56df10c

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  
+    static capitalize(string) {
+    	return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(string) {
+    	return string.replace(/[^A-Za-z0-9- ']+/g, "")
+    }
+
+	static titleize(string) {
+		let arr = string.split(' ')
+		let excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+		// console.log(arr)
+		const arr2 = arr.map((word, i)  => {
+			return excludeWords.includes(word) && i!=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1)
+		})
+		return arr2.join(' ')
+	}
 }
